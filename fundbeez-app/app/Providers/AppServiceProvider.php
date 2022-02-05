4e06a69753df1875f9f51d5152be07aa00c3e819
HiddenSeat <faishalnursetiawan@gmail.com>
2022-02-05 17:04:54 +0700

    custom business route model binding

--- fundbeez-app/app/Providers/AppServiceProvider.php
+++ fundbeez-app/app/Providers/AppServiceProvider.php
@@ -8,6 +8,9 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Lang;
+use App\Models\Enums\InvestmentStatus;
+use App\Models\Investment;
+use Illuminate\Support\Facades\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,15 +32,19 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->setVerificationMailingTemplate();
+        Route::bind('business', function ($value) {
+            return Investment::where(['id' => $value, 'status' => InvestmentStatus::ACCEPT])->firstOrFail();
+        });
     }
 
-    private function setVerificationMailingTemplate(){
-        VerifyEmail::toMailUsing(function ($notifiable, $url){
+    private function setVerificationMailingTemplate()
+    {
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
             return (new MailMessage)
-            ->subject(Lang::get('Verify Email Address'))
-            ->line(Lang::get('Please click the button below to verify your email address.'))
-            ->action(Lang::get('Verify Email Address'), $url)
-            ->line(Lang::get('If you did not create an account, no further action is required.'));
+                ->subject(Lang::get('Verify Email Address'))
+                ->line(Lang::get('Please click the button below to verify your email address.'))
+                ->action(Lang::get('Verify Email Address'), $url)
+                ->line(Lang::get('If you did not create an account, no further action is required.'));
         });
     }
 }
